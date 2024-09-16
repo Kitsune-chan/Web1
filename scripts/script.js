7ba9ed97cb837f49b48d0ebcc0ca2f59d2a36352
Neko /ᐠ - ˕ -マ Ⳋ <drudf2@mail.ru>
2024-09-16 20:30:23 +0700

    new

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// var myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello World";
+
+
+// document.querySelector("body").onclick = function () {
+//     alert("Ouch! Stop poking me!");
+//   };
